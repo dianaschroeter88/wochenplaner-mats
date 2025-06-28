24ee2725ed74fcbd8846e09113886a35d18fa5a8
dianaschroeter88 <dianaschroeter@hotmail.com>
2025-06-28 18:04:17 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "wochenplaner-cache-v1";
+const urlsToCache = [
+  "adhs_wochenplaner.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
